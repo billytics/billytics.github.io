8577b7256ea6449680b827eea9ca9c0f880df618
btabanpour <btabanpour@gmail.com>
2014-10-11 20:24:53 -0400

    isotope

--- js/move_receipts.js
+++ js/move_receipts.js
@@ -3,7 +3,7 @@
 $(document).ready(function () {
 
   jQuery.getJSON( "receipt_data/dashboard_receipt_data.json", function( data ) {
-      $('#receiptContainer').append('<div id="items" class="packery-container" style="clear: both" row=""></div>');
+      $('#receiptContainer').append('<div id="items" class="isotope" style="clear: both" row=""></div>');
 
       $.each( data, function( index, element ) {
         // console.log(element);
@@ -13,15 +13,50 @@ $(document).ready(function () {
         $('#items').append(compiled(element));
       });
      
-      packery();
+      // packery();
+      isotope();
       modalReceipt(data);
   });
-    
-    
-    
-
 
 })
+
+function isotope() {
+
+    var $container = $('.isotope');
+    $container.isotope({
+        layoutMode:'fitRows',
+        itemSelector: 'item',
+        sortBy: 'original-order',
+        // columnWidth: 330,
+        // rowHeight: 100,
+        // gutter: 2
+        getSortData: {
+          merchant: '.merchant',
+          cardType: '.cardType',
+          total: '.total parseInt',
+          trial: '.trial',
+          // date: function( itemElem ) {
+          //   // var weight = $( itemElem ).find('.weight').text();
+          //   // return parseFloat( weight.replace( /[\(\)]/g, '') );
+          // }
+        }
+    });
+
+     // bind sort button click
+      $('#sorts').on( 'click', 'button', function() {
+        var sortByValue = $(this).attr('data-sort-by');
+        $container.isotope({ sortBy: sortByValue });
+      });
+
+      // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+}
 
 function packery() {
     var pckry;
@@ -43,6 +78,8 @@ function packery() {
 
     pckry = $container.data('packery');
 }
+
+
 
 function modalReceipt(data) {
     $('.receiptModalButton').on('click',function() {
